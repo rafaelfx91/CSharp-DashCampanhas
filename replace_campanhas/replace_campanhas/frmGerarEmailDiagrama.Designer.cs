da1659712b8b401ef153e8aa24631aadf5f7e904
Rafael <rafaelmfox@gmail.com>
2023-06-23 09:38:30 -0300

    PlusoftDash
    ...

--- replace_campanhas/replace_campanhas/frmGerarEmailDiagrama.Designer.cs
+++ replace_campanhas/replace_campanhas/frmGerarEmailDiagrama.Designer.cs
@@ -32,15 +32,30 @@
             this.btnLimpar = new System.Windows.Forms.Button();
             this.btnSair = new System.Windows.Forms.Button();
             this.btnGerar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkJessica = new System.Windows.Forms.CheckBox();
+            this.chkCarol = new System.Windows.Forms.CheckBox();
+            this.chkBarbara = new System.Windows.Forms.CheckBox();
+            this.chkCamila = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chkSms = new System.Windows.Forms.CheckBox();
+            this.chkEmail = new System.Windows.Forms.CheckBox();
+            this.chkPush = new System.Windows.Forms.CheckBox();
+            this.gbBoasVindas = new System.Windows.Forms.GroupBox();
+            this.rdrBomdia = new System.Windows.Forms.RadioButton();
+            this.rdrBoatarde = new System.Windows.Forms.RadioButton();
+            this.rdrBoanoite = new System.Windows.Forms.RadioButton();
             this.label1 = new System.Windows.Forms.Label();
             this.txtSaida = new System.Windows.Forms.TextBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.gbBoasVindas.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtCopiar
             // 
             this.txtCopiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCopiar.Location = new System.Drawing.Point(116, 506);
+            this.txtCopiar.Location = new System.Drawing.Point(127, 506);
             this.txtCopiar.Name = "txtCopiar";
             this.txtCopiar.Size = new System.Drawing.Size(98, 43);
             this.txtCopiar.TabIndex = 45;
@@ -50,7 +65,7 @@
             // btnLimpar
             // 
             this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLimpar.Location = new System.Drawing.Point(220, 506);
+            this.btnLimpar.Location = new System.Drawing.Point(238, 506);
             this.btnLimpar.Name = "btnLimpar";
             this.btnLimpar.Size = new System.Drawing.Size(98, 43);
             this.btnLimpar.TabIndex = 44;
@@ -60,12 +75,13 @@
             // btnSair
             // 
             this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSair.Location = new System.Drawing.Point(324, 506);
+            this.btnSair.Location = new System.Drawing.Point(361, 506);
             this.btnSair.Name = "btnSair";
             this.btnSair.Size = new System.Drawing.Size(98, 43);
             this.btnSair.TabIndex = 43;
             this.btnSair.Text = "Sair";
             this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
             // 
             // btnGerar
             // 
@@ -76,43 +92,185 @@
             this.btnGerar.TabIndex = 42;
             this.btnGerar.Text = "Gerar";
             this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkCamila);
+            this.groupBox1.Controls.Add(this.chkBarbara);
+            this.groupBox1.Controls.Add(this.chkCarol);
+            this.groupBox1.Controls.Add(this.chkJessica);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(145, 152);
+            this.groupBox1.TabIndex = 49;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Solicitante";
+            // 
+            // chkJessica
+            // 
+            this.chkJessica.AutoSize = true;
+            this.chkJessica.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.chkJessica.Location = new System.Drawing.Point(10, 19);
+            this.chkJessica.Name = "chkJessica";
+            this.chkJessica.Size = new System.Drawing.Size(88, 24);
+            this.chkJessica.TabIndex = 0;
+            this.chkJessica.Text = "Jessica";
+            this.chkJessica.UseVisualStyleBackColor = true;
+            // 
+            // chkCarol
+            // 
+            this.chkCarol.AutoSize = true;
+            this.chkCarol.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.chkCarol.Location = new System.Drawing.Point(10, 49);
+            this.chkCarol.Name = "chkCarol";
+            this.chkCarol.Size = new System.Drawing.Size(70, 24);
+            this.chkCarol.TabIndex = 1;
+            this.chkCarol.Text = "Carol";
+            this.chkCarol.UseVisualStyleBackColor = true;
+            // 
+            // chkBarbara
+            // 
+            this.chkBarbara.AutoSize = true;
+            this.chkBarbara.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.chkBarbara.Location = new System.Drawing.Point(10, 79);
+            this.chkBarbara.Name = "chkBarbara";
+            this.chkBarbara.Size = new System.Drawing.Size(92, 24);
+            this.chkBarbara.TabIndex = 2;
+            this.chkBarbara.Text = "Barbara";
+            this.chkBarbara.UseVisualStyleBackColor = true;
+            // 
+            // chkCamila
+            // 
+            this.chkCamila.AutoSize = true;
+            this.chkCamila.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.chkCamila.Location = new System.Drawing.Point(10, 109);
+            this.chkCamila.Name = "chkCamila";
+            this.chkCamila.Size = new System.Drawing.Size(82, 24);
+            this.chkCamila.TabIndex = 3;
+            this.chkCamila.Text = "Camila";
+            this.chkCamila.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chkSms);
+            this.groupBox2.Controls.Add(this.chkEmail);
+            this.groupBox2.Controls.Add(this.chkPush);
+            this.groupBox2.Location = new System.Drawing.Point(163, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(145, 152);
+            this.groupBox2.TabIndex = 50;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Disparo";
+            // 
+            // chkSms
+            // 
+            this.chkSms.AutoSize = true;
+            this.chkSms.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.chkSms.Location = new System.Drawing.Point(10, 79);
+            this.chkSms.Name = "chkSms";
+            this.chkSms.Size = new System.Drawing.Size(66, 24);
+            this.chkSms.TabIndex = 2;
+            this.chkSms.Text = "SMS";
+            this.chkSms.UseVisualStyleBackColor = true;
+            // 
+            // chkEmail
+            // 
+            this.chkEmail.AutoSize = true;
+            this.chkEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.chkEmail.Location = new System.Drawing.Point(10, 49);
+            this.chkEmail.Name = "chkEmail";
+            this.chkEmail.Size = new System.Drawing.Size(82, 24);
+            this.chkEmail.TabIndex = 1;
+            this.chkEmail.Text = "EMAIL";
+            this.chkEmail.UseVisualStyleBackColor = true;
+            // 
+            // chkPush
+            // 
+            this.chkPush.AutoSize = true;
+            this.chkPush.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.chkPush.Location = new System.Drawing.Point(10, 19);
+            this.chkPush.Name = "chkPush";
+            this.chkPush.Size = new System.Drawing.Size(77, 24);
+            this.chkPush.TabIndex = 0;
+            this.chkPush.Text = "PUSH";
+            this.chkPush.UseVisualStyleBackColor = true;
+            // 
+            // gbBoasVindas
+            // 
+            this.gbBoasVindas.Controls.Add(this.rdrBomdia);
+            this.gbBoasVindas.Controls.Add(this.rdrBoatarde);
+            this.gbBoasVindas.Controls.Add(this.rdrBoanoite);
+            this.gbBoasVindas.Location = new System.Drawing.Point(314, 12);
+            this.gbBoasVindas.Name = "gbBoasVindas";
+            this.gbBoasVindas.Size = new System.Drawing.Size(145, 152);
+            this.gbBoasVindas.TabIndex = 51;
+            this.gbBoasVindas.TabStop = false;
+            // 
+            // rdrBomdia
+            // 
+            this.rdrBomdia.AutoSize = true;
+            this.rdrBomdia.Checked = true;
+            this.rdrBomdia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdrBomdia.Location = new System.Drawing.Point(17, 19);
+            this.rdrBomdia.Name = "rdrBomdia";
+            this.rdrBomdia.Size = new System.Drawing.Size(92, 24);
+            this.rdrBomdia.TabIndex = 31;
+            this.rdrBomdia.TabStop = true;
+            this.rdrBomdia.Text = "Bom dia";
+            this.rdrBomdia.UseVisualStyleBackColor = true;
+            // 
+            // rdrBoatarde
+            // 
+            this.rdrBoatarde.AutoSize = true;
+            this.rdrBoatarde.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdrBoatarde.Location = new System.Drawing.Point(17, 49);
+            this.rdrBoatarde.Name = "rdrBoatarde";
+            this.rdrBoatarde.Size = new System.Drawing.Size(106, 24);
+            this.rdrBoatarde.TabIndex = 32;
+            this.rdrBoatarde.Text = "Boa tarde";
+            this.rdrBoatarde.UseVisualStyleBackColor = true;
+            // 
+            // rdrBoanoite
+            // 
+            this.rdrBoanoite.AutoSize = true;
+            this.rdrBoanoite.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdrBoanoite.Location = new System.Drawing.Point(17, 79);
+            this.rdrBoanoite.Name = "rdrBoanoite";
+            this.rdrBoanoite.Size = new System.Drawing.Size(104, 24);
+            this.rdrBoanoite.TabIndex = 33;
+            this.rdrBoanoite.Text = "Boa noite";
+            this.rdrBoanoite.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(571, 162);
+            this.label1.Location = new System.Drawing.Point(164, 167);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(144, 20);
-            this.label1.TabIndex = 48;
+            this.label1.TabIndex = 53;
             this.label1.Text = "Mensagem saida";
             // 
             // txtSaida
             // 
             this.txtSaida.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSaida.Location = new System.Drawing.Point(428, 175);
+            this.txtSaida.Location = new System.Drawing.Point(12, 190);
             this.txtSaida.Multiline = true;
             this.txtSaida.Name = "txtSaida";
-            this.txtSaida.Size = new System.Drawing.Size(382, 263);
-            this.txtSaida.TabIndex = 47;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(268, 100);
-            this.groupBox1.TabIndex = 49;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Solicitante";
+            this.txtSaida.Size = new System.Drawing.Size(447, 310);
+            this.txtSaida.TabIndex = 52;
             // 
             // frmGerarEmailDiagrama
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(860, 561);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(477, 561);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtSaida);
+            this.Controls.Add(this.gbBoasVindas);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.txtCopiar);
             this.Controls.Add(this.btnLimpar);
             this.Controls.Add(this.btnSair);
@@ -120,6 +278,12 @@
             this.Name = "frmGerarEmailDiagrama";
             this.Text = "frmGerarEmailDiagrama";
             this.Load += new System.EventHandler(this.frmGerarEmailDiagrama_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.gbBoasVindas.ResumeLayout(false);
+            this.gbBoasVindas.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -131,8 +295,20 @@
         private System.Windows.Forms.Button btnLimpar;
         private System.Windows.Forms.Button btnSair;
         private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chkCamila;
+        private System.Windows.Forms.CheckBox chkBarbara;
+        private System.Windows.Forms.CheckBox chkCarol;
+        private System.Windows.Forms.CheckBox chkJessica;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox chkSms;
+        private System.Windows.Forms.CheckBox chkEmail;
+        private System.Windows.Forms.CheckBox chkPush;
+        private System.Windows.Forms.GroupBox gbBoasVindas;
+        private System.Windows.Forms.RadioButton rdrBomdia;
+        private System.Windows.Forms.RadioButton rdrBoatarde;
+        private System.Windows.Forms.RadioButton rdrBoanoite;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtSaida;
-        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
--- replace_campanhas/replace_campanhas/frmGerarEmailDiagrama.cs
+++ replace_campanhas/replace_campanhas/frmGerarEmailDiagrama.cs
@@ -20,6 +20,59 @@ namespace replace_campanhas
         private void frmGerarEmailDiagrama_Load(object sender, EventArgs e)
         {
             this.MaximizeBox = false;
+            setBoasVindas(DateTime.Now.ToString("HH"));
+        }
+
+        public void setBoasVindas(string hora)
+        {
+            try
+            {
+                var horas = int.Parse(hora);
+                if ((horas >= 00) && (horas < 12))
+                    rdrBomdia.Checked = true;
+                if ((horas >= 12) && (horas < 19))
+                    rdrBoatarde.Checked = true;
+                if ((horas >= 19) && (horas < 00))
+                    rdrBoanoite.Checked = true;
+            }
+            catch (Exception ex)
+            {
+                rdrBoanoite.Checked = true;
+            }
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            var boasVindas = String.Empty;
+
+            if (rdrBomdia.Checked)
+                boasVindas = "Bom dia!";
+            if (rdrBoatarde.Checked)
+                boasVindas = "Boa tarde!";
+            if (rdrBoanoite.Checked)
+                boasVindas = "Bboa noite!";
+
+
+            var Solicitante = String.Empty;
+            var SolicitanteEmail = String.Empty;
+
+            if (chkJessica.Checked)
+            {
+                
+            }
+
+
+
+            var mensagemSaida = ""+
+                                "";
+
+
+
         }
     }
 }
