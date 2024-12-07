29a6340317b1801eb0ec192e4dd8cbedb0a0c6c4
Rafael <rafaelmfox@gmail.com>
2024-12-07 08:27:28 -0300

    Save
    save

--- replace_campanhas/replace_campanhas/frmValidaFrasePushV3Solicitante.Designer.cs
+++ replace_campanhas/replace_campanhas/frmValidaFrasePushV3Solicitante.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnMostrarEmote = new System.Windows.Forms.Button();
             this.lblCaracterMsg = new System.Windows.Forms.Label();
             this.lblCaracterTitulo = new System.Windows.Forms.Label();
             this.btnLimpar = new System.Windows.Forms.Button();
@@ -40,27 +39,17 @@
             this.rdClaroResidencial = new System.Windows.Forms.RadioButton();
             this.pbPushLogo = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnVisualizar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pbPushLogo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btnMostrarEmote
-            // 
-            this.btnMostrarEmote.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMostrarEmote.Location = new System.Drawing.Point(12, 335);
-            this.btnMostrarEmote.Name = "btnMostrarEmote";
-            this.btnMostrarEmote.Size = new System.Drawing.Size(208, 43);
-            this.btnMostrarEmote.TabIndex = 74;
-            this.btnMostrarEmote.Text = "Mostrar emote";
-            this.btnMostrarEmote.UseVisualStyleBackColor = true;
-            this.btnMostrarEmote.Click += new System.EventHandler(this.btnMostrarEmote_Click);
             // 
             // lblCaracterMsg
             // 
             this.lblCaracterMsg.AutoSize = true;
             this.lblCaracterMsg.BackColor = System.Drawing.Color.LightGray;
             this.lblCaracterMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCaracterMsg.Location = new System.Drawing.Point(168, 153);
+            this.lblCaracterMsg.Location = new System.Drawing.Point(163, 253);
             this.lblCaracterMsg.Name = "lblCaracterMsg";
             this.lblCaracterMsg.Size = new System.Drawing.Size(354, 25);
             this.lblCaracterMsg.TabIndex = 68;
@@ -71,7 +60,7 @@
             this.lblCaracterTitulo.AutoSize = true;
             this.lblCaracterTitulo.BackColor = System.Drawing.Color.LightGray;
             this.lblCaracterTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCaracterTitulo.Location = new System.Drawing.Point(163, 66);
+            this.lblCaracterTitulo.Location = new System.Drawing.Point(163, 64);
             this.lblCaracterTitulo.Name = "lblCaracterTitulo";
             this.lblCaracterTitulo.Size = new System.Drawing.Size(280, 25);
             this.lblCaracterTitulo.TabIndex = 66;
@@ -80,7 +69,7 @@
             // btnLimpar
             // 
             this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLimpar.Location = new System.Drawing.Point(226, 335);
+            this.btnLimpar.Location = new System.Drawing.Point(143, 304);
             this.btnLimpar.Name = "btnLimpar";
             this.btnLimpar.Size = new System.Drawing.Size(98, 43);
             this.btnLimpar.TabIndex = 65;
@@ -91,7 +80,7 @@
             // btnSair
             // 
             this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSair.Location = new System.Drawing.Point(330, 335);
+            this.btnSair.Location = new System.Drawing.Point(247, 304);
             this.btnSair.Name = "btnSair";
             this.btnSair.Size = new System.Drawing.Size(68, 43);
             this.btnSair.TabIndex = 64;
@@ -103,7 +92,7 @@
             // 
             this.txtTituloEntrada.BackColor = System.Drawing.Color.LightGray;
             this.txtTituloEntrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTituloEntrada.Location = new System.Drawing.Point(168, 94);
+            this.txtTituloEntrada.Location = new System.Drawing.Point(168, 92);
             this.txtTituloEntrada.Name = "txtTituloEntrada";
             this.txtTituloEntrada.Size = new System.Drawing.Size(719, 31);
             this.txtTituloEntrada.TabIndex = 60;
@@ -113,12 +102,13 @@
             // 
             this.txtMsgEntrada.BackColor = System.Drawing.Color.LightGray;
             this.txtMsgEntrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMsgEntrada.Location = new System.Drawing.Point(168, 181);
+            this.txtMsgEntrada.Location = new System.Drawing.Point(168, 129);
             this.txtMsgEntrada.Multiline = true;
             this.txtMsgEntrada.Name = "txtMsgEntrada";
             this.txtMsgEntrada.Size = new System.Drawing.Size(719, 121);
             this.txtMsgEntrada.TabIndex = 62;
             this.txtMsgEntrada.TextChanged += new System.EventHandler(this.txtMsgEntrada_TextChanged);
+            this.txtMsgEntrada.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtMsgEntrada_KeyDown);
             // 
             // rdClaroPay
             // 
@@ -138,7 +128,7 @@
             // 
             this.rdMinahClaroMovel.AutoSize = true;
             this.rdMinahClaroMovel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
-            this.rdMinahClaroMovel.Location = new System.Drawing.Point(209, 13);
+            this.rdMinahClaroMovel.Location = new System.Drawing.Point(237, 12);
             this.rdMinahClaroMovel.Name = "rdMinahClaroMovel";
             this.rdMinahClaroMovel.Size = new System.Drawing.Size(285, 29);
             this.rdMinahClaroMovel.TabIndex = 76;
@@ -150,7 +140,7 @@
             // 
             this.rdClaroResidencial.AutoSize = true;
             this.rdClaroResidencial.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold);
-            this.rdClaroResidencial.Location = new System.Drawing.Point(500, 13);
+            this.rdClaroResidencial.Location = new System.Drawing.Point(549, 13);
             this.rdClaroResidencial.Name = "rdClaroResidencial";
             this.rdClaroResidencial.Size = new System.Drawing.Size(338, 29);
             this.rdClaroResidencial.TabIndex = 77;
@@ -161,7 +151,7 @@
             // pbPushLogo
             // 
             this.pbPushLogo.Image = global::replace_campanhas.Properties.Resources.unnamed;
-            this.pbPushLogo.Location = new System.Drawing.Point(24, 94);
+            this.pbPushLogo.Location = new System.Drawing.Point(24, 92);
             this.pbPushLogo.Name = "pbPushLogo";
             this.pbPushLogo.Size = new System.Drawing.Size(138, 137);
             this.pbPushLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -173,20 +163,31 @@
             this.pictureBox1.Image = global::replace_campanhas.Properties.Resources.ProjetoPush4;
             this.pictureBox1.Location = new System.Drawing.Point(12, 48);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(897, 281);
+            this.pictureBox1.Size = new System.Drawing.Size(897, 250);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 72;
             this.pictureBox1.TabStop = false;
+            // 
+            // btnVisualizar
+            // 
+            this.btnVisualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVisualizar.Location = new System.Drawing.Point(12, 304);
+            this.btnVisualizar.Name = "btnVisualizar";
+            this.btnVisualizar.Size = new System.Drawing.Size(125, 43);
+            this.btnVisualizar.TabIndex = 78;
+            this.btnVisualizar.Text = "Visualizar";
+            this.btnVisualizar.UseVisualStyleBackColor = true;
+            this.btnVisualizar.Click += new System.EventHandler(this.btnVisualizar_Click);
             // 
             // frmValidaFrasePushV3Solicitante
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(918, 386);
+            this.ClientSize = new System.Drawing.Size(909, 351);
+            this.Controls.Add(this.btnVisualizar);
             this.Controls.Add(this.rdClaroResidencial);
             this.Controls.Add(this.rdMinahClaroMovel);
             this.Controls.Add(this.rdClaroPay);
-            this.Controls.Add(this.btnMostrarEmote);
             this.Controls.Add(this.pbPushLogo);
             this.Controls.Add(this.lblCaracterMsg);
             this.Controls.Add(this.lblCaracterTitulo);
@@ -196,7 +197,7 @@
             this.Controls.Add(this.txtMsgEntrada);
             this.Controls.Add(this.pictureBox1);
             this.Name = "frmValidaFrasePushV3Solicitante";
-            this.Text = "frmValidaFrasePushV3Solicitante";
+            this.Text = "Push";
             this.Load += new System.EventHandler(this.frmValidaFrasePushV3Solicitante_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pbPushLogo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -206,8 +207,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnMostrarEmote;
         private System.Windows.Forms.PictureBox pbPushLogo;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label lblCaracterMsg;
@@ -219,5 +218,6 @@
         private System.Windows.Forms.RadioButton rdClaroPay;
         private System.Windows.Forms.RadioButton rdMinahClaroMovel;
         private System.Windows.Forms.RadioButton rdClaroResidencial;
+        private System.Windows.Forms.Button btnVisualizar;
     }
 }
--- replace_campanhas/replace_campanhas/frmValidaFrasePushV3Solicitante.cs
+++ replace_campanhas/replace_campanhas/frmValidaFrasePushV3Solicitante.cs
@@ -5,9 +5,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace replace_campanhas
 {
@@ -21,11 +23,12 @@ namespace replace_campanhas
 
         private void frmValidaFrasePushV3Solicitante_Load(object sender, EventArgs e)
         {
-
+            this.MaximizeBox = false;
         }
 
-
+        //
         //METODOS
+        //
         public void mudarImagemPush()
         {
             if(rdClaroPay.Checked)
@@ -38,7 +41,6 @@ namespace replace_campanhas
             pbPushLogo.SizeMode = PictureBoxSizeMode.StretchImage;
 
         }
-
         public void tamanhoTitulo()
         {
             int totalValue = 0;
@@ -68,6 +70,58 @@ namespace replace_campanhas
                 lblCaracterMsg.ForeColor = Color.Green;
 
         }
+        public void contarCaracter(System.Windows.Forms.TextBox textBox, System.Windows.Forms.Label label, string mensagem, int limite )
+        {
+            // Define o limite de caracteres
+            int LimiteCaracteres = limite;
+
+            //Define a mensagel da label
+            string labelMensagem = mensagem;
+
+            // Contador de caracteres
+            int contador = 0;
+            int i = 0;
+            string texto = textBox.Text;
+
+            while (i < texto.Length)
+            {
+                // Verifica se há uma sequência {XXXXX}
+                if (texto[i] == '{')
+                {
+                    int fimChave = texto.IndexOf('}', i);
+                    if (fimChave != -1)
+                    {
+                        // Ignora toda a sequência {XXXXX} e conta como 1 caractere
+                        contador++;
+                        i = fimChave + 1; // Avança para depois da chave de fechamento
+                    }
+                    else
+                    {
+                        // Se não houver '}', conta o caractere '{' normalmente
+                        contador++;
+                        i++;
+                    }
+                }
+                else
+                {
+                    // Conta caracteres normais
+                    contador++;
+                    i++;
+                }
+            }
+
+            // Atualiza o texto do Label com a contagem
+            label.Text = $"{labelMensagem}: {contador}/{LimiteCaracteres}";
+
+            // Atualiza a cor do Label com base no limite
+            if (contador > LimiteCaracteres)
+                label.ForeColor = Color.Red; // Excedeu o limite
+            else if (contador == 0)
+                label.ForeColor = Color.Black;
+            else
+                label.ForeColor = Color.Green; // Dentro do limite
+
+        }
 
 
 
@@ -79,68 +133,99 @@ namespace replace_campanhas
 
 
 
-
-
-
+        //
+        //BOTOES
+        //
         private void rdClaroPay_CheckedChanged(object sender, EventArgs e)
         {
             mudarImagemPush();
         }
-
         private void txtTituloEntrada_TextChanged(object sender, EventArgs e)
         {
-            tamanhoTitulo();
+            contarCaracter(txtTituloEntrada, lblCaracterTitulo, "Caracter no titulo", 50);
         }
-
         private void txtMsgEntrada_TextChanged(object sender, EventArgs e)
         {
-            tamanhoMensagem();
-        }
+            //tamanhoMensagem();
+            contarCaracter(txtMsgEntrada,lblCaracterMsg,"Caracter na mensagem",200);
 
+
+        }
         private void btnMostrarEmote_Click(object sender, EventArgs e)
         {
             LimparString ls = new LimparString();
             string btnMostrar = "Mostrar emote";
             string btnEsconder = "Esconder emote";
-            string btnGerar = btnMostrarEmote.Text;
+            //string btnGerar = btnMostrarEmote.Text;
 
 
-            switch (btnGerar)
+            switch (btnMostrar)
             {
                 case "Mostrar emote":
                     txtTituloEntrada.Text = ls.CodigoParaEmote(txtTituloEntrada.Text);
                     txtMsgEntrada.Text = ls.CodigoParaEmote(txtMsgEntrada.Text);
-                    btnMostrarEmote.Text = btnEsconder;
+                   // btnMostrarEmote.Text = btnEsconder;
                     break;
                 case "Esconder emote":
                     txtTituloEntrada.Text = ls.EmoteParaCodigo2(txtTituloEntrada.Text);
                     txtMsgEntrada.Text = ls.EmoteParaCodigo2(txtMsgEntrada.Text);
-                    btnMostrarEmote.Text = btnMostrar;
+                    //btnMostrarEmote.Text = btnMostrar;
                     break;
                 default:
                     break;
             }
-
-            
-
-
-
-
-
-
         }
-
-
-
         private void btnLimpar_Click(object sender, EventArgs e)
         {
             txtMsgEntrada.Clear();
             txtTituloEntrada.Clear();
         }
-
         private void btnSair_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+        private void txtMsgEntrada_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Verifica se a tecla pressionada foi "Enter"
+            if (e.KeyCode == Keys.Enter)
+            {
+                // Cancela o comportamento padrão do Enter (como mudança de foco)
+                e.SuppressKeyPress = true;
+
+                // Obtém o TextBox que gerou o evento
+                System.Windows.Forms.TextBox textBox = sender as System.Windows.Forms.TextBox;
+
+                if (textBox != null)
+                {
+                    // Adiciona {000A} seguido de uma nova linha
+                    int cursorPosition = textBox.SelectionStart;
+                    string novaLinha = "{000A}" + Environment.NewLine;
+                    textBox.Text = textBox.Text.Insert(cursorPosition, novaLinha);
+
+                    // Move o cursor para o início da nova linha
+                    textBox.SelectionStart = cursorPosition + novaLinha.Length;
+                }
+            }
+        }
+        private void btnVisualizar_Click(object sender, EventArgs e)
+        {
+
+            if (txtTituloEntrada.BorderStyle == BorderStyle.None)
+            {
+                txtTituloEntrada.BorderStyle = BorderStyle.Fixed3D;
+                txtMsgEntrada.BorderStyle = BorderStyle.Fixed3D;
+                lblCaracterMsg.Visible = true;
+                lblCaracterTitulo.Visible = true;
+            }
+            else
+            {
+                txtTituloEntrada.BorderStyle = BorderStyle.None;
+                txtMsgEntrada.BorderStyle = BorderStyle.None;
+                lblCaracterMsg.Visible = false;
+                lblCaracterTitulo.Visible = false;
+            }
+
+
         }
     }
 }
